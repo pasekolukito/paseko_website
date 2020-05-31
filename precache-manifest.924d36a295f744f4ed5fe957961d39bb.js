39f4baf786efbed59ea52e0b557d682790318f82
Lukasz Pasek <lukasz.pasek@volvocars.com>
2020-05-31 15:59:06 -0700

    Updates

---
+++ precache-manifest.924d36a295f744f4ed5fe957961d39bb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c4d64ec61660f650c84b4d2a337b605",
+    "url": "/paseko_website/index.html"
+  },
+  {
+    "revision": "219131d50723f88b27dc",
+    "url": "/paseko_website/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ad075e68631f83e25fbf",
+    "url": "/paseko_website/static/js/2.2f5ef2c2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/paseko_website/static/js/2.2f5ef2c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "219131d50723f88b27dc",
+    "url": "/paseko_website/static/js/main.b05b3233.chunk.js"
+  },
+  {
+    "revision": "92ea26939d1876276133",
+    "url": "/paseko_website/static/js/runtime-main.092259ff.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/paseko_website/static/media/logo.5d5d9eef.svg"
+  }
+]);
